139b8fe18973c96eba31299f00ee4748fc411007
Jan Rychtar <honza.rychtar@gmail.com>
2017-09-18 00:06:25 +0200

    Add routing, navigation and some empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,37 @@ import { MaterializeModule } from 'ng2-materialize';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ViewerComponent } from './viewer/viewer.component';
+import { ViewerComponent } from './book/viewer/viewer.component';
+import { RouterModule } from '@angular/router';
+
+import { BookComponent } from './book/book.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { CollectionsComponent } from './collections/collections.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ViewerComponent
+    ViewerComponent,
+    BookComponent,
+    HelpComponent,
+    HomeComponent,
+    BrowseComponent,
+    CollectionsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterializeModule.forRoot()
+    MaterializeModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'collections', component: CollectionsComponent },
+      { path: 'browse', component: BrowseComponent },
+      { path: 'help', component: HelpComponent },
+      { path: 'documents/:uuid', component: BookComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
