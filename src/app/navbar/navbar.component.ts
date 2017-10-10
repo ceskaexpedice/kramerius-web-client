f38cf7a5b85c6ddf796733e01f93bf4b381000de
samba <pkudela@searchtechnologies.com>
2017-10-10 10:18:07 +0200

    Created search bar component, ts code moved from navbar component to the searchbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,4 @@
-import { LibrarySearchService } from './../services/library-search.service';
-import { Router } from '@angular/router';
-import { Component, OnInit, Input } from '@angular/core';
-import { Translator } from 'angular-translator';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,41 +7,9 @@ import { Translator } from 'angular-translator';
 })
 export class NavbarComponent implements OnInit {
 
-  @Input() autocomplete;
-  @Input() input;
-
-  searchStr;
-
-  constructor(public translator: Translator,
-    public router: Router,
-    public service: LibrarySearchService) {
-  }
+  constructor() { }
 
   ngOnInit() {
   }
-
-  onSelected(event) {
-    if (event) {
-      const uuid = event['originalObject']['PID'];
-      const title = event['title'];
-      this.router.navigate(['/search'], { queryParams: { q: title } });
-    }
-  }
-
-  onLanguageChanged(lang: string) {
-    localStorage.setItem('lang', lang);
-    this.translator.language = lang;
-  }
-
-  onKeyUp(event) {
-    if (event.keyCode === 13) {
-      let q = this.searchStr;
-      if (q == null) {
-        q = '';
-      }
-      this.router.navigate(['/search'], { queryParams: { q: q } });
-    }
-  }
-
 
 }
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,49 @@
+import { LibrarySearchService } from './../services/library-search.service';
+import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+import { Translator } from 'angular-translator'
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit {
+
+  @Input() autocomplete;
+  @Input() input;
+
+  searchStr;
+
+  constructor(public translator: Translator,
+    public router: Router,
+    public service: LibrarySearchService) {
+  }
+
+  ngOnInit() {
+  }
+
+  onSelected(event) {
+    if (event) {
+      const uuid = event['originalObject']['PID'];
+      const title = event['title'];
+      this.router.navigate(['/search'], { queryParams: { q: title } });
+    }
+  }
+
+  onLanguageChanged(lang: string) {
+    localStorage.setItem('lang', lang);
+    this.translator.language = lang;
+  }
+
+  onKeyUp(event) {
+    if (event.keyCode === 13) {
+      let q = this.searchStr;
+      if (q == null) {
+        q = '';
+      }
+      this.router.navigate(['/search'], { queryParams: { q: q } });
+    }
+  }
+
+}
