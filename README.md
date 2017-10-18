d3b6aa3cad8e80a8febc1feaa87fe9b039ffc309
samba <pkudela@searchtechnologies.com>
2017-10-18 15:42:08 +0200

    Doc type bar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # KrameriusClient
+
+## poznamky
+- HONZA -> krizek u search boxu by mel byt viditelny pouze v pripade, kdyz je v nem zadano query, nebo pokud se v nem pise
+- HONZA -> query nezustava v searchboxu
+- PETR -> responsivni chovani -> pri zmensenem okne horni casti doctype facety zmizi a zobrazi se standardne v levem facetovem sloupci
+- PETR -> dodelat doctype bar (mozna rozdelit na 2 komponenty)
--- src/app/search/search-filters-doctype/search-filters-doctype.component.ts
+++ src/app/search/search-filters-doctype/search-filters-doctype.component.ts
@@ -11,10 +11,10 @@ export class SearchFiltersDoctypeComponent implements OnInit {
  doctype = [
    {id: 1, title: "Knihy", type: "monograph", count: "117614", "active": true},
    {id: 2, title: "Novinky a časopisy", type: "periodical", count: "3576"},
-   {id: 3, title: "Audio", type: "soundrecording", count: "2224"},
+   {id: 3, title: "Audio", type: "soundrecording", count: "2224", "active": true},
    {id: 4, title: "Mapy", type: "map", count: "1008"},
-   {id: 5, title: "Grafika", type: "graphic", count: "610", "active": true},
-   {id: 6, title: "Hudebniny", type: "sheetmusis", count: "549"},
+   {id: 5, title: "Grafika", type: "graphic", count: "610"},
+   {id: 6, title: "Hudebniny", type: "sheetmusic", count: "549", "active": true},
    {id: 7, title: "Archiválie", type: "archive", count: "297"},
    {id: 8, title: "Rukopisy", type: "manuscript", count: "103"}
  ]
