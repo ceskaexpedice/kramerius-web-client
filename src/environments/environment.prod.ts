a848eb5e3b1b5022a226552655fd1368407249ed
Jan Rychtar <honza.rychtar@gmail.com>
2018-02-19 13:06:05 +0100

    Add config to the production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,42 @@
 export const environment = {
-  production: true
+  production: true,
+  // MZK
+  title: 'Moravská zemská knihovna',
+  logo: 'http://registr.digitalniknihovna.cz/libraries/mzk/logo',
+  url: 'https://kramerius.mzk.cz',
+  solr: {
+    joinedDoctypes: true,
+    facetTruncate: false,
+    doctypes: ['monograph', 'periodical', 'map', 'graphic', 'archive', 'manuscript', 'soundrecording', 'sheetmusic']
+  }
+
+  // KNAV
+  // title: 'Knihovna Akademie věd ČR',
+  // logo: 'http://registr.digitalniknihovna.cz/libraries/knav/logo',
+  // url: 'https://kramerius.lib.cas.cz',
+  // solr: {
+  //   joinedDoctypes: false,
+  //   facetTruncate: true,
+  //   doctypes: ['monograph', 'periodical']
+  // }
+
+  // NKP
+  // title: 'Národní knihovna České republiky',
+  // logo: 'http://registr.digitalniknihovna.cz/libraries/nkp/logo',
+  // url: 'http://kramerius4.nkp.cz',
+  // solr: {
+  //   joinedDoctypes: false,
+  //   facetTruncate: true,
+  //   doctypes: ['monograph', 'periodical', 'map', 'sheetmusic']
+  // }
+
+  // MLP
+  // title: 'Městská knihovna v Praze',
+  // logo: 'http://registr.digitalniknihovna.cz/libraries/mlp/logo',
+  // url: 'http://kramerius4.mlp.cz',
+  // solr: {
+  //   joinedDoctypes: true,
+  //   facetTruncate: false,
+  //   doctypes: ['monograph', 'periodical', 'soundrecording', 'sheetmusic']
+  // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,24 +6,24 @@
 export const environment = {
   production: false,
   // MZK
-  // title: 'Moravská zemská knihovna',
-  // logo: 'http://registr.digitalniknihovna.cz/libraries/mzk/logo',
-  // url: 'https://kramerius.mzk.cz',
-  // solr: {
-  //   joinedDoctypes: true,
-  //   facetTruncate: false,
-  //   doctypes: ['monograph', 'periodical', 'map', 'graphic', 'archive', 'manuscript', 'soundrecording', 'sheetmusic']
-  // }
-
-  // KNAV
-  title: 'Knihovna Akademie věd ČR',
-  logo: 'http://registr.digitalniknihovna.cz/libraries/knav/logo',
-  url: 'https://kramerius.lib.cas.cz',
+  title: 'Moravská zemská knihovna',
+  logo: 'http://registr.digitalniknihovna.cz/libraries/mzk/logo',
+  url: 'https://kramerius.mzk.cz',
   solr: {
-    joinedDoctypes: false,
-    facetTruncate: true,
+    joinedDoctypes: true,
+    facetTruncate: false,
     doctypes: ['monograph', 'periodical', 'map', 'graphic', 'archive', 'manuscript', 'soundrecording', 'sheetmusic']
   }
+
+  // KNAV
+  // title: 'Knihovna Akademie věd ČR',
+  // logo: 'http://registr.digitalniknihovna.cz/libraries/knav/logo',
+  // url: 'https://kramerius.lib.cas.cz',
+  // solr: {
+  //   joinedDoctypes: false,
+  //   facetTruncate: true,
+  //   doctypes: ['monograph', 'periodical']
+  // }
 
   // NKP
   // title: 'Národní knihovna České republiky',
@@ -32,10 +32,10 @@ export const environment = {
   // solr: {
   //   joinedDoctypes: false,
   //   facetTruncate: true,
-  //   doctypes: ['monograph', 'periodical', 'map', 'graphic', 'archive', 'manuscript', 'soundrecording', 'sheetmusic']
+  //   doctypes: ['monograph', 'periodical', 'map', 'sheetmusic']
   // }
 
-  // MKP
+  // MLP
   // title: 'Městská knihovna v Praze',
   // logo: 'http://registr.digitalniknihovna.cz/libraries/mlp/logo',
   // url: 'http://kramerius4.mlp.cz',
