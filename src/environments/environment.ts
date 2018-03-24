eb71d196820c516c335f14c9f24352e7812511b0
Jan Rychtar <honza.rychtar@gmail.com>
2018-03-24 21:17:14 +0100

    clean environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,54 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  // MZK
-  title: 'Moravská zemská knihovna',
-  logo: 'http://registr.digitalniknihovna.cz/libraries/mzk/logo',
-  url: 'https://kramerius.mzk.cz',
-  solr: {
-    joinedDoctypes: true,
-    facetTruncate: true,
-    doctypes: ['monograph', 'periodical', 'map', 'graphic', 'archive', 'manuscript', 'soundrecording', 'sheetmusic']
-  }
-
-  // KNAV
-  // title: 'Knihovna Akademie věd ČR',
-  // logo: 'http://registr.digitalniknihovna.cz/libraries/knav/logo',
-  // url: 'https://kramerius.lib.cas.cz',
-  // solr: {
-  //   joinedDoctypes: false,
-  //   facetTruncate: true,
-  //   doctypes: ['monograph', 'periodical']
-  // }
-
-  // NKP
-  // title: 'Národní knihovna České republiky',
-  // logo: 'http://registr.digitalniknihovna.cz/libraries/nkp/logo',
-  // url: 'http://kramerius4.nkp.cz',
-  // solr: {
-  //   joinedDoctypes: false,
-  //   facetTruncate: true,
-  //   doctypes: ['monograph', 'periodical', 'map', 'sheetmusic']
-  // }
-
-  // MLP
-  // title: 'Městská knihovna v Praze',
-  // logo: 'http://registr.digitalniknihovna.cz/libraries/mlp/logo',
-  // url: 'http://kramerius4.mlp.cz',
-  // solr: {
-  //   joinedDoctypes: true,
-  //   facetTruncate: false,
-  //   doctypes: ['monograph', 'periodical', 'soundrecording', 'sheetmusic']
-  // }
-
-  // NM - zvuk
-  // title: 'Národní muzeum - Zvuk',
-  // logo: 'http://registr.digitalniknihovna.cz/libraries/nmzv/logo',
-  // url: 'http://zvuk.nm.cz',
-  // solr: {
-  //   joinedDoctypes: true,
-  //   facetTruncate: false,
-  //   doctypes: ['monograph', 'periodical', 'map', 'graphic', 'archive', 'manuscript', 'soundrecording', 'sheetmusic']
-  // }
+  production: false
 };
