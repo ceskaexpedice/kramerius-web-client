bb1c71466d278a8fb8289dfd5a31f7a158ae85dc
Jan Rychtar <honza.rychtar@gmail.com>
2019-03-28 11:11:25 +0100

    Oprava generovani odkazu pro citaci a sdileni

--- src/app/services/share.service.ts
+++ src/app/services/share.service.ts
@@ -14,18 +14,17 @@ export class ShareService {
     const query = location.search;
     let uuid: string;
     if (path.indexOf('uuid:') > -1) {
-      uuid = path.substr(path.indexOf('uuid:'), 41);
+      uuid = path.substr(path.indexOf('uuid:'));
+    }
+    if (query.indexOf('article=uuid:') > -1) {
+      uuid = this.parseUuid(query, 'article');
+    }
+    if (query.indexOf('page=uuid:') > -1) {
+      uuid = this.parseUuid(query, 'page');
     }
     if (!uuid) {
       return;
     }
-    if (query.indexOf('article=uuid:') > -1) {
-      uuid = query.substr(query.indexOf('article=uuid:') + 8, 49);
-    }
-    if (query.indexOf('page=uuid:') > -1) {
-      uuid = query.substr(query.indexOf('page=uuid:') + 5, 46);
-    }
-
     let url: string;
     if (this.appSettings.share_url) {
       if (this.appSettings.multiKramerius) {
@@ -43,6 +42,13 @@ export class ShareService {
     return url;
   }
 
+  private parseUuid(query: string, param: string) {
+    for (const p of query.split('&')) {
+      if (p.indexOf(param + '=') > -1) {
+        return p.substring(p.indexOf(param + '=') + param.length + 1);
+      }
+    }
+  }
 
   public showShareDialog() {
     const link = this.getPagePersistentLink();
