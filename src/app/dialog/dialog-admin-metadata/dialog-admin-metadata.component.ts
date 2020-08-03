04ab51a2c090e5c0ea83eb48fcce5f42c31c786a
Jan Rychtar <honza.rychtar@gmail.com>
2020-08-03 11:38:16 +0200

    Preklad hlasky o chybejichim resource v metadatech

--- src/app/dialog/dialog-admin-metadata/dialog-admin-metadata.component.ts
+++ src/app/dialog/dialog-admin-metadata/dialog-admin-metadata.component.ts
@@ -4,6 +4,7 @@ import { Metadata } from '../../model/metadata.model';
 import { KrameriusApiService } from '../../services/kramerius-api.service';
 import { parseString, processors, Builder } from 'xml2js';
 import { SolrService } from '../../services/solr.service';
+import { Translator } from 'angular-translator';
 
 
 @Component({
@@ -17,7 +18,7 @@ export class DialogAdminMetadataComponent extends MzBaseModal implements OnInit 
 
   resource = 'mods';
 
-  constructor(private api: KrameriusApiService) {
+  constructor(private api: KrameriusApiService, private translator: Translator) {
     super();
   }
 
@@ -54,7 +55,11 @@ export class DialogAdminMetadataComponent extends MzBaseModal implements OnInit 
 
   reload() {
     if (this.selection.tab === 'page' && this.resource === 'alto' && !this.metadata.isPublic) {
-      this.selection[this.resource] = `Objekt ${this.selection.tab.toUpperCase()} neobsahuje ${this.resource.toUpperCase()}`;
+      this.selection[this.resource] = String(this.translator.instant('metadata-dialog.missing', 
+      { 
+        model: this.selection.tab.toUpperCase(),
+        resource: this.resource.toUpperCase()
+      }));
       return;
     } 
     if (!this.selection[this.resource]) {
@@ -76,7 +81,11 @@ export class DialogAdminMetadataComponent extends MzBaseModal implements OnInit 
         }
       },
       () => {
-        this.selection[this.resource] = `Objekt ${this.selection.tab.toUpperCase()} neobsahuje ${this.resource.toUpperCase()}`;
+        this.selection[this.resource] = String(this.translator.instant('metadata-dialog.missing', 
+        { 
+          model: this.selection.tab.toUpperCase(),
+          resource: this.resource.toUpperCase()
+        }));
       });
     }
   }
