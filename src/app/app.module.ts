70bd09e7b408b244631577171bc0a9b39955d1f9
Jan Rychtar <honza.rychtar@gmail.com>
2021-02-25 19:13:02 +0100

    Verze 2.0.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -276,7 +276,7 @@ export function hljsLanguages() {
       providedLanguages: ['en', 'cs'],
       defaultLanguage: 'cs',
       loaderOptions: {
-        path: 'assets/i18n/{{language}}.json?v2.0.4'
+        path: 'assets/i18n/{{language}}.json?v2.0.5'
       }
     }),
     MzButtonModule,
