5f90a4e6f61a5fe17009830c68e085a2d7352424
Jan Rychtar <honza.rychtar@gmail.com>
2022-01-10 17:31:51 +0100

    Prepsany login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,11 @@
 import { AuthService } from '../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { User } from '../model/user.model';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   
