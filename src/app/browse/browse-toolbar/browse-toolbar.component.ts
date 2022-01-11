a33d8fb3e9a5cf6ff3d80c9f20b7947a4b9b6337
Jan Rychtar <honza.rychtar@gmail.com>
2022-01-11 11:06:53 +0100

    Prepracovany toolbar u prochazeni

--- src/app/browse/browse-toolbar/browse-toolbar.component.ts
+++ src/app/browse/browse-toolbar/browse-toolbar.component.ts
@@ -4,7 +4,8 @@ import { BrowseService } from './../../services/browse.service';
 
 @Component({
   selector: 'app-browse-toolbar',
-  templateUrl: './browse-toolbar.component.html'
+  templateUrl: './browse-toolbar.component.html',
+  styleUrls: ['./browse-toolbar.component.scss']
 })
 export class BrowseToolbarComponent implements OnInit {
 
