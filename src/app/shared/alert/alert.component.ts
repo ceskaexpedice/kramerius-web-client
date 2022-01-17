43f35efcdfbd1d2dc728c6aac65db9064d3579b7
Jan Rychtar <honza.rychtar@gmail.com>
2022-01-17 18:46:49 +0100

    Opravy u epub, pdf

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -2,30 +2,18 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
-  templateUrl: './alert.component.html'
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent implements OnInit {
+  
   @Input() type;
   @Input() label;
   @Input() message;
-  @Input() description;
-  @Input() html;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  getIcon() {
-    if (this.type === 'warning') {
-      return 'alert';
-    } else if (this.type === 'danger') {
-      return 'alert-circle';
-    } else if (this.type === 'info') {
-      return 'information';
-    } else if (this.type === 'success') {
-      return 'check';
-    }
   }
 
 }
