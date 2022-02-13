ab4199df4e3217eacf46afb8ecefb82bc4c86312
Jan Rychtar <honza.rychtar@gmail.com>
2022-02-13 17:56:33 +0100

    Oprava chybneho zobrazeni konvolutu u casti kovolutu

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -488,7 +488,9 @@ export class BookService {
         const pages = [];
         const parents = [];
         for (const p of inputPages) {
-            if (p['model'] === 'supplement' || (doctype == 'oldprintomnibusvolume' && p['model'] != 'page')) {
+            console.log('AAAAA', p);
+
+            if (p['model'] === 'supplement' || (doctype == 'oldprintomnibusvolume' && p['model'] != 'oldprintomnibusvolume' && p['model'] != 'page')) {
                 parents.push(p);
                 this.extraParents.push(p);
             } else {
