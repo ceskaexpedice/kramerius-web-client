b3ea32b71d7547d4f31f9eff0511c062e2ec5c0c
Jan Rychtar <honza.rychtar@gmail.com>
2022-02-14 17:28:04 +0100

    Placeholder pro jazyk klienta u linku na dnnt prihlaseni

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -55,7 +55,7 @@ export class NavbarComponent implements OnInit {
   login() {
     if (this.appSettings.auth) {
       this.analytics.sendEvent('navbar', 'login-shib');
-      const url = `${this.appSettings.auth.loginUrl}?target=${window.location.href}`;
+      const url = `${this.appSettings.auth.loginUrl.replace(/\${LANG}/, this.translate.currentLang)}?target=${window.location.href}`;
       window.open(url, '_top');
     } else if (this.appSettings.krameriusLogin && !this.authService.isLoggedIn()) {
       this.analytics.sendEvent('navbar', 'login');
