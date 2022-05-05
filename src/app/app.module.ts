92a1c3cfc19f4d34a791e709505ac7d8c1d4adbc
Jan Rychtar <honza.rychtar@gmail.com>
2022-05-05 11:09:21 +0200

    2.3.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -163,7 +163,7 @@ export function hljsLanguages() {
 }
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v2.3.4');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v2.3.5');
 }
 
 export function appInitializerFactory(translate: TranslateService) {
