f154df18a3c1aec8ea7ce68edd78ebf6904cf4b0
Jan Rychtar <honza.rychtar@gmail.com>
2023-09-28 12:44:04 +0200

    Oprava CachingInterceptor

--- src/app/services/caching-interceptor.service.ts
+++ src/app/services/caching-interceptor.service.ts
@@ -12,8 +12,7 @@ export class CachingInterceptor implements HttpInterceptor {
     private adminApi: AdminApiService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    if (req.url.startsWith(this.adminApi.getBaseUrl()) || req.url.startsWith("https://k7.inovatika.dev/search/api/client/v7.0/folders")) {
-    // if (req.url.startsWith(this.adminApi.getBaseUrl()))  {
+    if (req.method != 'GET' || req.url.startsWith(this.adminApi.getBaseUrl()) || req.url.includes("/search/api/client/v7.0/folders")) {
       return next.handle(req);
     }
     const cachedResponse = this.cache.get(req);
