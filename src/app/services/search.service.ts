ec325b5f402c4f9b3b48a31b339683f81b613551
pavluska <pavla.rychtarova@gmail.com>
2024-02-08 09:10:29 +0100

    odstraneni dotazu po 50 ve vyhledavani ve folders

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -128,24 +128,16 @@ export class SearchService {
             this.initAccess();
         }
         if (this.query.folder) {
-           this.folderService.getFolder(this.query.folder.uuid).subscribe(folder => {
+            this.folderService.getFolder(this.query.folder.uuid).subscribe(folder => {
                 this.folder = folder;
                 this.query.folder['name'] = this.folder.name;
-                this.query.folder['items'] = [];
-                let dividedItems = this.folderService.divideArray(this.folder.items[0], 50);
-                let i = 0;
-                for (const dividedItem of dividedItems) {
-                    let uuids = dividedItem.map(uuid => uuid.id);
-                    this.api.getSearchResults(this.solr.buildFolderItemsPathsQuery(uuids)).subscribe(response => {
-                        let items = response['response']['docs'].map(x => x.own_pid_path);
-                        this.query.folder['items'] = this.query.folder['items'].concat(items);
-                        i = i + 1;
-                        if (i == dividedItems.length) {   
-                            this.search();
-                        }
-                    });
-                }
-           });
+                let uuids = this.folder.items[0].map(uuid => uuid.id);
+                this.api.getSearchResults(this.solr.buildFolderItemsPathsQuery(uuids)).subscribe(response => {
+                    let items = response['response']['docs'].map(x => x.own_pid_path);
+                    this.query.folder['items'] = items;
+                    this.search();
+                });
+            });
         }
          else {
             this.search();
