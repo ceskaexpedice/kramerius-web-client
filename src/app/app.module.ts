41d5c9c386e353d8f106d1ef27a69952f8ecb650
Jan Rychtar <honza.rychtar@gmail.com>
2024-03-26 09:53:47 +0100

    Verze 2.7.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -188,7 +188,7 @@ export function hljsLanguages() {
 }
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v2.7.1');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v2.7.2');
 }
 
 export function appInitializerFactory(translate: TranslateService) {
