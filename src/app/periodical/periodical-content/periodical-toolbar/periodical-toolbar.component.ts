3e3c5f19cfa7e2f87ab55a6e48c486af921f93b5
Jan Rychtar <honza.rychtar@gmail.com>
2024-04-12 09:54:35 +0200

    Pristupnost: Vyber razeni u hledani a v periodiku

--- src/app/periodical/periodical-content/periodical-toolbar/periodical-toolbar.component.ts
+++ src/app/periodical/periodical-content/periodical-toolbar/periodical-toolbar.component.ts
@@ -1,7 +1,8 @@
 import { PeriodicalService } from './../../../services/periodical.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AnalyticsService } from '../../../services/analytics.service';
 import { AuthService } from '../../../services/auth.service';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-periodical-toolbar',
@@ -10,6 +11,9 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class PeriodicalToolbarComponent implements OnInit {
 
+  @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
+
+
   constructor(public periodicalService: PeriodicalService, 
               public analytics: AnalyticsService,
               public auth: AuthService) { }
@@ -17,10 +21,33 @@ export class PeriodicalToolbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  getOrderingOptions(): string[] {
+    if (this.periodicalService.orderingType === 'periodical') {
+      return ['earliest', 'latest'];
+    } else if (this.periodicalService.orderingType === 'fulltext') {
+      return ['relevance', 'earliest', 'latest'];
+    } else {
+      return [];
+    }
+  }
+
   selectLayout(layout: string) {
     this.analytics.sendEvent('periodical', 'layout', layout);
     this.periodicalService.changeActiveLayout(layout);
     this.periodicalService.activeMobilePanel = 'content';
+  }
+
+  changeOrdering(ordering: string) {
+    this.menuTrigger.closeMenu();
+    if (this.periodicalService.orderingType === 'periodical') {
+      if (ordering === 'earliest') {
+        this.periodicalService.setReverseOrder(false);
+      } else if (ordering === 'latest') {
+        this.periodicalService.setReverseOrder(true);
+      }
+    } else if (this.periodicalService.orderingType === 'fulltext') {
+      this.periodicalService.setOrdering(ordering);
+    } 
   }
 
 }
--- src/app/search/search-toolbar/search-toolbar.component.ts
+++ src/app/search/search-toolbar/search-toolbar.component.ts
@@ -1,10 +1,11 @@
 import { AppSettings } from './../../services/app-settings';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SearchService } from './../../services/search.service';
 import { AnalyticsService } from '../../services/analytics.service';
 import { AuthService } from '../../services/auth.service';
 import { CsvService } from '../../services/csv.service';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-search-toolbar',
@@ -14,6 +15,7 @@ import { LocalStorageService } from '../../services/local-storage.service';
 })
 export class SearchToolbarComponent implements OnInit {
 
+  @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
   devMode: boolean = false;
 
   constructor(
@@ -27,6 +29,20 @@ export class SearchToolbarComponent implements OnInit {
 
   ngOnInit() {
     this.devMode = this.localStorageService.getProperty(LocalStorageService.DEV_MODE) === '1';
+  }
+
+  getOrderingOptions(): string[] {
+    let options = [];
+    if (this.search.query.relevanceOrderingEnabled()) {
+      options.push('relevance');
+    }
+    options = options.concat(['alphabetical', 'newest', 'latest', 'earliest']);
+    return options;
+  }
+
+  changeOrdering(ordering: string) {
+    this.menuTrigger.closeMenu();
+    this.search.changeOrdering(ordering);
   }
 
   toggleFilters() {
