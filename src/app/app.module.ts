37563835d656ce6cb0825567e72a60b1b51fcf9a
Jan Rychtar <honza.rychtar@gmail.com>
2024-04-26 14:37:21 +0200

    Doplneni chybejicich prekladu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -189,7 +189,7 @@ export function hljsLanguages() {
 }
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v2.8.0.1');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v2.8.0.2');
 }
 
 export function appInitializerFactory(translate: TranslateService) {
